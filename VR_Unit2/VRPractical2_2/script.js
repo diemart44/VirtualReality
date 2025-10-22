17127e1323f7e4b9f7122905b85c439ae479112c
diemart44 <gibsonl@nycstudents.net>
2025-10-22 16:01:44 +0000

    update js

--- VR_Unit2/VRPractical2_2/script.js
+++ VR_Unit2/VRPractical2_2/script.js
@@ -1,5 +1,13 @@
 
 window.addEventListener("DOMContentLoaded",function() {
-
-    
+    scene = document.querySelector("a-scene"); 
+    rocket = document.querySelector("#rocket");
+    pokemonball = document.querySelector("#pokemonball")
+    loop();
 })
+
+function loop(){
+    
+    pokemonball.setAttribute("rotation",{x:1, y:0, z:0});
+    
+}
