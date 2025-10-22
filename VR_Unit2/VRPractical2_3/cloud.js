14e0d6e1043935b8536cea2fb53a0abdcd775b89
diemart44 <gibsonl@nycstudents.net>
2025-10-22 16:33:46 +0000

    update.js

---
+++ VR_Unit2/VRPractical2_3/cloud.js
@@ -0,0 +1,22 @@
+class Cloud {
+
+ constructor(x,y,z){
+    this.obj = document.createElement("a-entity");
+
+    let cloud1 =document.createElement("a-sphere");
+    cloud1.setAttribute("color","white");
+    cloud1.setAttribute("position", "2,5,0");
+    
+    let cloud2 = document.createElement("a-sphere");
+    cloud2.setAttribute("color","white");
+    cloud2.setAttribute("position","0,5,0");
+    cloud2.setAttribute("height","2")
+
+}
+  
+
+
+
+
+
+}
